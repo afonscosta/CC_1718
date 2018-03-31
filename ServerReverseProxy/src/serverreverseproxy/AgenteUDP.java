1aea2259257a370efd6ec79259b0ac4a736238d7
Afonso Costa <afonso.costa.15@gmail.com>
2018-03-31 12:16:37 +0100

    Estabelecida a comunicação entre o AgenteUDP e o MonitorUDP. MonitorUDP manda request em multicast e os AgenteUDP's respondem em unicast.

---
+++ ServerReverseProxy/src/serverreverseproxy/AgenteUDP.java
@@ -0,0 +1,56 @@
+package serverreverseproxy;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class AgenteUDP {
+    public static void main(String args[]) throws Exception
+    {
+        //Porta usada
+        int port = 8888;
+
+        //Endereço multicast
+        InetAddress group = InetAddress.getByName("239.8.8.8");
+
+        //Socket para o multicast
+        MulticastSocket s = new MulticastSocket(port);
+
+        //Junta-se ao grupo multicast para receber pacotes
+        s.joinGroup(group);
+
+        //Pacote que vai receber o pedido multicast
+        byte[] buf = new byte[1024];
+        DatagramPacket recv = new DatagramPacket(buf, buf.length);
+
+        //Recebe o pedido multicast
+        s.receive(recv);
+        System.out.println("Received data from: " + recv.getAddress().toString() +
+                ":" + recv.getPort() + " with length: " +
+                recv.getLength());
+        String receiveMsg = new String(recv.getData(),0,1024);
+        System.out.println("Data: " + receiveMsg);
+
+
+        /*
+            Envia resposta em unicast.
+
+            Since one can send unicast packets using the same MulticastSocket
+        instance as for ones multicasts, it makes sense to mention how unicasts
+        are handled when there is more than one listener, which can only be when
+        they are all on the same machine.
+            Unicast traffic sent to the port will be received by only one of the
+        listeners with a socket bound to the port. With my test setup, the last
+        socket to bind to the port receives the unicast traffic.
+        */
+        String sendData = "Resposta em unicast";
+        DatagramPacket sendPacket = new DatagramPacket(sendData.getBytes(), sendData.length(), recv.getAddress(), 8888);
+        s.send(sendPacket);
+
+        //Sai do grupo
+        s.leaveGroup(group);
+
+        //Fecha o socket multicast
+        s.close();
+    }
+}
